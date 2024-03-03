fef67903dad2e5ab11b9b3a8cc872392e6685313
muda <64138881+syihab06@users.noreply.github.com>
2024-03-03 18:54:38 +0700

    Update back.sh

--- back.sh
+++ back.sh
@@ -1,1 +1,9 @@
-fastboot 
+adb devices
+adb reboot bootloader
+fastboot --disable -verity flash vbmeta vbmeta.img
+fastboot --disable -verification flash vbmeta_system vbmeta_system.img
+fastboot erase system
+fastboot flash system system.img
+fastboot -w
+fastboot reboot recovery
+echo wipe data lalu reboot
